2529116374c0608bf4133b057bd273e5a95982db
Oskar Buhl <oskarbuhl@holmkjaer.dk>
2022-02-20 22:08:06 +0000

    Upload New File

---
+++ minix2/sketch_2.js
@@ -0,0 +1,25 @@
+let num = 2000;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(170, 198, 230);
+  fill(255,0,0)
+  noStroke()
+  ellipse(550,200,200)
+  ellipse(250,200,200)
+}
+function draw() {
+
+  stroke(255);
+  strokeWeight(2)
+  if (mouseIsPressed === true) {
+    variableEllipse(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
+function variableEllipse(x, y, px, py){
+  let speed = abs(x-px) + abs(y-py);
+  stroke(speed);
+  fill(255);
+  noStroke();
+  ellipse(x,y,speed,speed);
+}
